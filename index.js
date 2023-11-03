774f19cb1735bc12b0fa3745b44a4b238b046d2d
itsmohamedyahia <justmyahia@gmail.com>
2023-11-03 23:03:36 +0200

    connect pscale to app

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 
-const app = express();
+//db
+require("dotenv").config();
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
+// express and cors
+const app = express();
 app.use(cors());
 
+// db connect
+connection.connect();
+
+app.get("/api/data", (req, res) => {
+  connection.query("SELECT * FROM mcq_questions", function (err, rows, fields) {
+    if (err) throw err;
+
+    res.send(rows);
+  });
+});
+
+// routes and spinning
 app.get("/api/user/firstName", (req, res) => {
   res.send("Yahia FROM BACKEND BABY");
   res.status(200);
