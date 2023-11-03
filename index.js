d3bf4cb6dd50c53c958fbe8ff92f8d33ac186e06
itsmohamedyahia <justmyahia@gmail.com>
2023-11-04 00:59:27 +0200

    refractor and router

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const connection = require("./db").connection;
 
-//db
-require("dotenv").config();
-const mysql = require("mysql2");
-const connection = mysql.createConnection(process.env.DATABASE_URL);
-
-// express and cors
 const app = express();
+const router = express.Router();
+
 app.use(cors());
 
-// db connect
-connection.connect();
+app.use("/api", router);
 
-app.get("/api/data", (req, res) => {
+router.get("/data", (req, res) => {
   connection.query("SELECT * FROM mcq_questions", function (err, rows, fields) {
     if (err) throw err;
 
@@ -21,8 +17,7 @@ app.get("/api/data", (req, res) => {
   });
 });
 
-// routes and spinning
-app.get("/api/user/firstName", (req, res) => {
+router.get("/user/firstName", (req, res) => {
   res.send("Yahia FROM BACKEND BABY");
   res.status(200);
 });
